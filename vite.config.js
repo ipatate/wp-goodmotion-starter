d63f42d800ba3f6666d79ee89c74820a1a104e25
ipatate <faramazpat@gmail.com>
2021-06-03 13:04:14 +0200

    chore: correction assets base for dev

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
-import { resolve, sep } from "path";
-import { defineConfig } from "vite";
-import liveReload from "vite-plugin-live-reload";
+import { resolve, sep } from 'path'
+import { defineConfig } from 'vite'
+import liveReload from 'vite-plugin-live-reload'
 
 // find theme dir name
-const _path = process.cwd().split(sep);
-const themeDir = _path[_path.length - 1];
+const _path = process.cwd().split(sep)
+const themeDir = _path[_path.length - 1]
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [liveReload([__dirname + "/**/*.php", __dirname + "/**/*.twig"])],
-  base: `/wp-content/themes/${themeDir}/dist/`,
-  root: "",
+  plugins: [liveReload([__dirname + '/**/*.php', __dirname + '/**/*.twig'])],
+  base:
+    process.env.APP_ENV === 'development'
+      ? `/wp-content/themes/${themeDir}/`
+      : `/wp-content/themes/${themeDir}/dist/`,
+  root: '',
   build: {
     // output dir for production build
-    outDir: resolve(__dirname, "dist"),
+    outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
     manifest: true,
-    target: "es5",
+    target: 'es5',
     rollupOptions: {
-      input: resolve(__dirname, "main.js"),
+      input: resolve(__dirname, 'main.js'),
     },
   },
   server: {
@@ -27,12 +30,12 @@ export default defineConfig({
     port: 3000,
     https: false,
     hmr: {
-      host: "localhost",
+      host: 'localhost',
     },
   },
   resolve: {
     alias: {
-      vue: "vue/dist/vue.esm-bundler.js",
+      vue: 'vue/dist/vue.esm-bundler.js',
     },
   },
-});
+})
