fbd5c030f8b72b808a0d94ceec0fbdcb38d90eb2
ipatate <faramazpat@gmail.com>
2021-06-11 10:54:05 +0200

    feat: clean template, clean css. Finaly use only prose on bloc text globally

--- home.php
+++
@@ -1,7 +0,0 @@
-<?php /* Template Name: Home page Template */
-
-$context = Timber::context();
-
-$timber_post     = new Timber\Post();
-$context['post'] = $timber_post;
-Timber::render(array('home.twig', 'page.twig'), $context);
